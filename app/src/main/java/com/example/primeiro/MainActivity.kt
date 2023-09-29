aa1282a861bb4fb611b7fe2bd2d9418412515234
81784244368 <paulo.silva-junior@serpro.gov.br>
2023-09-29 09:59:57 -0300

    termino do codelab

--- app/src/main/java/com/example/primeiro/MainActivity.kt
+++ app/src/main/java/com/example/primeiro/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.primeiro
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,11 +15,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,13 +38,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.primeiro.ui.theme.PrimeiroTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -50,12 +62,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.M)
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
 
     var deveMostrarOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    Surface(modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
         if (deveMostrarOnboarding) {
             OnboardingScreen(onContinueClicked = { deveMostrarOnboarding = false })
         } else {
@@ -86,6 +99,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit,
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.M)
 @Composable
 private fun Saudacoes(modifier: Modifier = Modifier,
         names: List<String> = List(1000) {"$it"}//listOf("Mundo", "Compose")
@@ -97,41 +111,67 @@ private fun Saudacoes(modifier: Modifier = Modifier,
         }
 }
 
+@RequiresApi(Build.VERSION_CODES.M)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
+    Card (
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name = name)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+@Composable
+fun CardContent(name: String, modifier: Modifier = Modifier) {
     //definindo um estado
     val expanded = remember { mutableStateOf(false) }
 
-    //val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    val extraPadding by animateDpAsState (
-          if (expanded.value) 48.dp else 0.dp,
-          animationSpec = spring(
-              dampingRatio = Spring.DampingRatioMediumBouncy,
-              stiffness = Spring.StiffnessLow
-          )
-    )
-
-
-    Surface(color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Row (Modifier.padding(24.dp)) {
-            Column (
-                Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))){
-                Text(text = "Oi,")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
                     fontWeight = FontWeight.ExtraBold
-                ))
+                )
+            )
+            if (expanded.value) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Bem-vindo" else "Saudação")
-            }
+        }
+        IconButton(onClick = { expanded.value = !expanded.value }) {
+            androidx.compose.material3.Icon(
+                imageVector = if (expanded.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expanded.value) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.M)
 @Preview(
     showBackground = true,
     widthDp = 360,
@@ -148,6 +188,7 @@ fun DefaultPreview() {
 }
 
 
+@RequiresApi(Build.VERSION_CODES.M)
 @Preview(showBackground = true, widthDp = 360)
 @Composable
 fun MyAppPreview() {
